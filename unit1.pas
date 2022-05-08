8eb05d304ef70184f99c1d8ea75184bc48870147
skvorecoo <odegov36@gmail.com>
2022-05-08 11:09:48 -0700

    +font change

--- unit1.pas
+++ unit1.pas
@@ -5,30 +5,42 @@ unit Unit1;
 interface
 
 uses
-  Classes, SysUtils, Forms, Controls, Graphics, Dialogs, Menus, StdCtrls;
+  Classes, SysUtils, Forms, Controls, Graphics, Dialogs, Menus, StdCtrls,
+  PrintersDlgs, Printers;
 
 type
 
   { TForm1 }
 
   TForm1 = class(TForm)
+    FontDialog: TFontDialog;
     MainMenu: TMainMenu;
     FileActions: TMenuItem;
     Editing: TMenuItem;
     CopyText: TMenuItem;
     Memo: TMemo;
+    MenuItem1: TMenuItem;
+    FontStyle: TMenuItem;
+    Style: TMenuItem;
+    PrinterSetupDialog: TPrinterSetupDialog;
+    PrintFile: TMenuItem;
     OpenDialog: TOpenDialog;
     PasteText: TMenuItem;
     CutText: TMenuItem;
     OpenFile: TMenuItem;
+    PrintDialog: TPrintDialog;
     SaveDialog: TSaveDialog;
     SaveFile: TMenuItem;
     SaveFileAs: TMenuItem;
     TerminateTextEditor: TMenuItem;
     procedure CopyTextClick(Sender: TObject);
     procedure CutTextClick(Sender: TObject);
+    procedure FontStyleClick(Sender: TObject);
+    procedure MemoChange(Sender: TObject);
+    procedure MenuItem1Click(Sender: TObject);
     procedure OpenFileClick(Sender: TObject);
     procedure PasteTextClick(Sender: TObject);
+    procedure PrintFileClick(Sender: TObject);
     procedure SaveFileAsClick(Sender: TObject);
     procedure SaveFileClick(Sender: TObject);
     procedure TerminateTextEditorClick(Sender: TObject);
@@ -63,6 +75,32 @@ begin
   Memo.CutToClipboard;
 end;
 
+procedure TForm1.FontStyleClick(Sender: TObject);
+begin
+  if FontDialog.Execute then Memo.Font:=FontDialog.Font;
+end;
+
+procedure TForm1.MemoChange(Sender: TObject);
+begin
+
+end;
+
+procedure TForm1.MenuItem1Click(Sender: TObject);
+begin
+  If not(Memo.WordWrap) then
+  begin
+    Memo.WordWrap:=True;
+    Memo.ScrollBars:=ssVertical;
+    MenuItem1.Checked:=True;
+  end
+  else
+  begin
+    Memo.WordWrap:=false;
+    Memo.ScrollBars:=ssBoth;
+    MenuItem1.Checked:=False;
+  end;
+end;
+
 procedure TForm1.OpenFileClick(Sender: TObject);
 begin
   if Form1.OpenDialog.Execute then
@@ -74,6 +112,11 @@ end;
 procedure TForm1.PasteTextClick(Sender: TObject);
 begin
   Memo.PasteFromClipboard;
+end;
+
+procedure TForm1.PrintFileClick(Sender: TObject);
+begin
+
 end;
 
 procedure PSaveFileAs;
@@ -94,6 +137,4 @@ begin
   if FileName='' then PSaveFileAs else Memo.Lines.SaveToFile(FileName);
 end;
 
-
 end.
-
